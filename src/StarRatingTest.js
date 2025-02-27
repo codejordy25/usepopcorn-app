6aa6df5aa9483ea5162a81d7c9b9b834a263daf9
codejordy25 <jordytsimba11@gmail.com>
2025-02-27 14:13:07 +0100

    271412116 Creating  Star

--- src/StarRatingTest.js
+++ src/StarRatingTest.js
@@ -67,7 +67,7 @@ function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
     >
       {full ? (
         <svg
-          xmlns="http://www.w3.org/2000/svg"
+          xsmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
           fill={color}
           stroke={color}
